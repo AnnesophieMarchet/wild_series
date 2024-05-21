7b043c186d96ac1c5b2705cd5ee6acd261460c47
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-21 17:01:12 +0200

    creation route.js programs

---
+++ server/app/controllers/sayActions.js
@@ -0,0 +1,7 @@
+const sayWelcome = (req, res) => {
+  res.send("Welcome to Wild Series anne !");
+};
+
+// Export it to import it somewhere else
+
+module.exports = { sayWelcome };
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -1,15 +1,23 @@
+/* ************************************************************************* */
+// Import And Use Routers Here
+/* ************************************************************************* */
 const express = require("express");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Import And Use Routers Here
-/* ************************************************************************* */
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
+// const { browse } = require("../../controllers/programActions");
+
+// router.get("/", browse);
 
 const itemsRouter = require("./items/router");
+const programsRouter = require("./programs/router");
 
 router.use("/items", itemsRouter);
-
+router.use("/programs", programsRouter);
 /* ************************************************************************* */
 
 module.exports = router;
