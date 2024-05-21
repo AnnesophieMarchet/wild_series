4d43401420af78f2c0763d0afa7b9ba42341ed04
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-05-21 14:51:38 +0200

    fix console.log with console.info

--- server/index.js
+++ server/index.js
@@ -6,13 +6,13 @@ const app = express();
 const port = process.env.APP_PORT;
 
 app.get("/", (req, res) => {
-  res.send("Welcome to Wild Series coucou c' est Anne so !");
+  res.send("Welcome to Wild Series coucou c' est nul  !");
 });
 
 // Route par défaut pour les erreurs 404
-app.use((req, res) => {
-  res.status(404).send("Page not found");
-});
+// app.use((req, res) => {
+//   res.status(404).send("Page not found");
+// });
 
 app.listen(port, () => {
   console.info(`Server is running on http://localhost:${port}`);
