78035ededaa08957f9c1ebd81a9a734da4b4e621
Anne-sophie Marchet <annesophiemarchet@gmail.com>
2024-06-02 11:55:30 +0200

    quete 06 - Harmonia : Créer ton premier Repository

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,4 +1,5 @@
 // Some data to make the trick
+const tables = require("../../database/tables");
 
 const categories = [
   {
@@ -11,16 +12,22 @@ const categories = [
   },
 ];
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = categories.filter((program) =>
-      program.synopsis.includes(req.query.q)
-    );
+// const browse = (req, res) => {
+//   if (req.query.q != null) {
+//     const filteredPrograms = categories.filter((program) =>
+//       program.synopsis.includes(req.query.q)
+//     );
 
-    res.json(filteredPrograms);
-  } else {
-    res.json(categories);
-  }
+//     res.json(filteredPrograms);
+//   } else {
+//     res.json(categories);
+//   }
+// };
+
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
